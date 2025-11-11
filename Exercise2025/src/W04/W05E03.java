8785fb948183545c58c30807bdc185dfb8da2495
JonathanSia-2025 <25006171@siswa-old.um.edu.my>
2025-11-11 11:04:23 +0800

    e03

---
+++ Exercise2025/src/W04/W05E03.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package W04;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author MelodyCloud
+ */
+public class W05E03 {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        
+        System.out.print("Do you want to continue? (yes/no): ");
+        String response = sc.next();
+        while ( ! (response.equals("no"))){
+            System.out.print("Do you want to continue? (yes/no): ");
+            response = sc.next();
+        }
+    }
+}
