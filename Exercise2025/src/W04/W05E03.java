d0c8d478516ca9a3cf3e47fa7f299d18607bd42e
JonathanSia-2025 <25006171@siswa-old.um.edu.my>
2025-11-11 14:41:26 +0800

    update

--- Exercise2025/src/W04/W05E03.java
+++ Exercise2025/src/W04/W05E03.java
@@ -14,11 +14,27 @@ public class W05E03 {
     public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
         
-        System.out.print("Do you want to continue? (yes/no): ");
-        String response = sc.next();
-        while (! response.equals("yes") && ! (response.equals("no"))){
-            System.out.print("Enter yes or no only: ");
+        String response;
+        boolean valid = true;
+        
+        do {
+            if (valid){
+                System.out.print("Do you want to continue: ");
+                
+                
+            } else{
+                System.out.print("enter yes or no only: ");
+            }
             response = sc.next();
-        }
+            
+            if (response.equalsIgnoreCase("yes")){
+                valid = true;
+            } else {
+                valid = false;
+            }
+            
+            
+            
+        } while (!response.equalsIgnoreCase("No"));
     }
 }
