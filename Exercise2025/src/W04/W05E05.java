c6a3bf78a6760a0c46128d400e9719499206f2a5
JonathanSia-2025 <25006171@siswa-old.um.edu.my>
2025-11-11 11:38:47 +0800

    E05

---
+++ Exercise2025/src/W04/W05E05.java
@@ -0,0 +1,36 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package W04;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author MelodyCloud
+ */
+public class W05E05 {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        
+        System.out.print("how many numbers will you enter?: ");
+        int n = sc.nextInt();
+        int countO = 0, countE = 0;
+        for (int i=0; i<n; i++){
+            System.out.printf("Enter number %d", i);
+            int number = sc.nextInt();
+            if (number % 2 == 0){
+                countE += 1;
+                
+            } else {
+                countO += 1;
+                
+            }
+        }
+        System.out.printf("Total even numbers: %d", countE);
+        System.out.println();
+        System.out.printf("Total odd numbers: %d", countO);
+
+    }
+}
